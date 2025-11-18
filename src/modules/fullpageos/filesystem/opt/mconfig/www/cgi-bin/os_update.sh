3e518bc37c8f92abbae67c0ff800e1dfea4f95ee
Yasseen Hilal <yasseenhilal@utexas.edu>
2025-11-18 16:09:08 -0600

    updater: added loading wait screen

--- src/modules/fullpageos/filesystem/opt/mconfig/www/cgi-bin/os_update.sh
+++ src/modules/fullpageos/filesystem/opt/mconfig/www/cgi-bin/os_update.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+STATE_FILE="/opt/mconfig/www/update.txt"
+
+# CGI header
 echo "Content-Type: text/plain"
 echo
 
+# Mark update as running
+echo "running" > "$STATE_FILE"
+
+# --- real update logic ---
 # Run as root (configure sudoers if invoked as www-data)
 # Default branch (devel); change or add logic for main/stable if you want
 LOG=$(/usr/local/bin/re3d-os-update 2>&1)
+rc=$?
+
+# Print logs back to the client
 echo "$LOG"
+
+# Update state file based on result
+if [ "$rc" -eq 0 ]; then
+  echo "reboot_required" > "$STATE_FILE"
+else
+  echo "error" > "$STATE_FILE"
+fi
+
+exit "$rc"
